cc3739ce9c30e54331f9bc20b0da809fa29262ee
kunal <kunalraturi1@gmail.com>
2024-03-30 19:21:22 +0530

    outputting dynamic image

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import reactImg from "./assets/react-core-concepts.png";
 const RandomDescription = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -8,7 +9,7 @@ function Header() {
   const description = RandomDescription[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
