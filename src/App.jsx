70b023ae323bdf7bb0b1370519fcc41850663f76
kunal <kunalraturi1@gmail.com>
2024-03-30 19:11:47 +0530

    outputting dynamic value

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+const RandomDescription = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = RandomDescription[getRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
