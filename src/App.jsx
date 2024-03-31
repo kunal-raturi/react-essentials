2150a8b186f2b4737533165e4589551971feb59e
kunal <kunalraturi1@gmail.com>
2024-03-31 13:22:07 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 const RandomDescription = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -18,12 +19,30 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt="title" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
